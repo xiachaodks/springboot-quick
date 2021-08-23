fc82caafe3558640a20736aac90f2f66c601d7f0
xiachao <xiachao625@126.com>
2021-08-23 15:58:38 +0800

    init springboot-quick project

---
+++ src/test/java/com/springbootquick/SpringbootQuickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootquick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootQuickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
